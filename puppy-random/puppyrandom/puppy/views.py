ec9e7a20143452dad1ca46837fe3b6f91294e341
Manutsanan Tothet <manutsanan.tot@gmail.com>
2022-07-01 10:51:29 +0700

    Created views and serialized

--- puppy-random/puppyrandom/puppy/views.py
+++ puppy-random/puppyrandom/puppy/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers
+
+from .models import Puppy
+
 # Create your views here.
+class PuppySerializer(serializers.Serializer):
+    name = serializers.CharField()
+    url = serializers.URLField()
+
+class PuppyListView(APIView):
+    def get(self, request):
+        puppies = Puppy.objects.all()
+        serialized = PuppySerializer(puppies, many=True)
+        data = serialized.data
+        return Response(data)
